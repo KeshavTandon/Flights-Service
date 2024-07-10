21ed63cb084149990c3f39cf3093165a72e85050
KeshavTandon <keshavtandon51@gmail.com>
2024-07-10 15:23:22 +0530

    Added new migration to add foreign key constraint in one to many realtion

--- src/index.js
+++ src/index.js
@@ -10,6 +10,16 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    const { City, Airport } =require('./models');
+    // const city=await City.findByPk(1);
+    // console.log(city);
+    // const kmpairport=await city.createAirport({name:'Kempegowda',code:'BLR'});
+    // console.log(kmpairport);
+    // await City.destroy({
+    //     where:{
+    //         id:9
+    //     }
+    // });
 });
--- src/models/airport.js
+++ src/models/airport.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
       this.belongsTo(models.City, {
         foreignKey: "cityId",
         onDelete: "CASCADE",
-        onUpdate: "CASCADE",
       });
     }
   }
